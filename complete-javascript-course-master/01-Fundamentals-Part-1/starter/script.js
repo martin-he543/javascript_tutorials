75d7d6dfbe87dc77bc0f84d1a3e2bda64b8e22c6
Martin He <martin.he543@outlook.com>
2024-06-28 17:04:26 +0100

    Updated JS from L1

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
+
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+
+let firstName = "Matilda";
+console.log(firstName);
+
+let PI = 3.1415;
+
+let country = "United Kingdom";
+let continent = "Europe";
+let population = 68.5e6;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
